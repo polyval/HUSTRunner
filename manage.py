47e76e32c9890a8a75eaf065ebaf0ab7e963dff3
polyval <zhoujiawei1216@gmail.com>
2016-04-20 16:13:33 +0800

    database migrations with flask-migrate

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 from flask_script import Manager, Server, Shell
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from app.user.models import User, Role
-from app.forum.models import Post, Topic
-
+from app.forum.models import Post, Topic, Comment
 
 app = create_app('testing')
 manager = Manager(app)
+migrate = Migrate(app, db)
 
 
 def make_shell_context():
@@ -15,8 +16,23 @@ def make_shell_context():
         >> app
         <Flask 'app'>
     """
-    return dict(app=app, db=db, User=User, Role=Role, Post=Post, Topic=Topic)
+    return dict(app=app, db=db, User=User,
+                Role=Role, Post=Post,
+                Topic=Topic, Comment=Comment)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def fake():
+    """Generate testing data"""
+    db.drop_all()
+    db.create_all()
+    Role.insert_roles()
+    Topic.insert_topic()
+    User.generate_fake()
+    Post.generate_fake()
+    Comment.generate_fake()
 
 
 @manager.command
